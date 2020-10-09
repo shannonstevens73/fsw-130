97f3963c36c83021ae4bfe4978a51e8f9477b93d
shannonstevens73 <shannon.stevens@bryanuniversity.edu>
2020-10-09 17:30:21 -0500

    first commit

---
+++ week2/context-practice2/src/bookContext.js
@@ -0,0 +1,83 @@
+import React, {Component} from "react"
+const {Provider, Consumer} = React.createContext()
+
+class BookContextProvider extends Component {
+    state = {
+        booklist: [
+            { 
+              fullName: "John Tolkien",
+              title: "The Lord of the Rings"
+            },
+      
+            { 
+              fullName: "Becky Eldredge",
+              title: "The Inner Chapel"
+            },
+      
+            { 
+              fullName: "Drew Karpyshyn",
+              title: "Rule of Two, Darth Bane 2"
+            }  
+          ],
+          
+        fullName: "",
+        title: ""
+    }
+
+    handleChange=(event) => {
+        event.preventDefault()
+        const {name, value} = event.target
+        this.setState({ 
+          [name]: value
+        })
+      }
+    
+      booklistUpdate=(event) => {
+        event.preventDefault()
+        this.setState({
+          booklist: this.state.booklist.concat({fullName: this.state.fullName, title: this.state.title})
+        })
+      }
+
+      bookEdit=(index) => {
+          console.log("edit", index)
+          console.log(document.getElementById(index).children)
+          var value1=(document.getElementById(index).children[4]).value
+          var value2=(document.getElementById(index).children[5]).value
+          var array=this.state.booklist
+          Object.assign(array[index], {fullName: value1, title: value2})
+          console.log(array)
+          this.setState({booklist: array})
+      }
+
+      bookDelete=(index) => {
+          console.log("edit", index)
+          console.log(document.getElementById(index))
+          var array=this.state.booklist
+          array.splice(index, 1)
+          console.log(array)
+          this.setState({booklist: array})
+      }
+
+  
+
+
+    render() {
+        return (
+            <Provider            
+                value={{
+                    booklist: this.state.booklist,
+                    fullName: this.state.fullName,
+                    title: this.state.title, 
+                    handleChange: this.handleChange,
+                    booklistUpdate: this.booklistUpdate,
+                    bookEdit: this.bookEdit,
+                    bookDelete: this.bookDelete
+                }}>
+                    {this.props.children}
+            </Provider>
+        )
+    }
+}
+
+export {BookContextProvider, Consumer as BookContextConsumer}
---
+++ week2/project1-ugly-things/my-app/src/index.js
@@ -0,0 +1,13 @@
+  
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import {BookContextProvider} from "./bookContext"
+import './index.css'
+
+ReactDOM.render(
+    <BookContextProvider>
+        <App />
+    </BookContextProvider>, 
+    document.getElementById('root')
+)
